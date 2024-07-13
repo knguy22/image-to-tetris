f0108efec7fee03364414d83ba40235619edacb4
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-13 14:18:43 -0700

    can use scores

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
-mod piece;
 mod board;
 mod draw;
+mod piece;
+mod score;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let board = tki();
     let skin = draw::BlockSkin::new("assets/HqGYC5G - Imgur.png")?;
     draw::draw_board(&board, &skin, "results/board.png");
+
+    let source_img = image::open("sources/rick-astley-890513150.jpg")?;
+    let source_img2 = image::open("sources/rick-astley2.jpg")?;
+    let result_img = image::open("results/board.png")?;
+
+    // rescale source_img to fit result_img
+    let resized_source_buffer = image::imageops::resize(&source_img, result_img.width(), result_img.height(), image::imageops::FilterType::Lanczos3);
+    let source_img = image::DynamicImage::from(resized_source_buffer);
+
+    let resized_source_buffer = image::imageops::resize(&source_img2, result_img.width(), result_img.height(), image::imageops::FilterType::Lanczos3);
+    let source_img2 = image::DynamicImage::from(resized_source_buffer);
+
+    let diff = score::compare_images(&source_img, &source_img)?;
+    println!("Source vs Source difference: {:?}", diff.score);
+
+    let diff = score::compare_images(&source_img, &result_img)?;
+    println!("Source vs Result difference: {:?}", diff.score);
+
+    let diff = score::compare_images(&source_img, &source_img2)?;
+    println!("Source vs Source2 difference: {:?}", diff.score);
+
     Ok(())
 }
 
