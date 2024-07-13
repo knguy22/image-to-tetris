db5b3549d42b91c308a8b69be24cee874f3c9331
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-13 13:46:04 -0700

    refactor the drawing

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@ mod piece;
 mod board;
 mod draw;
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    draw::split_blocks("assets/HqGYC5G - Imgur.png")?;
     let board = tki();
-    draw::draw_board(&board);
+    let skin = draw::BlockSkin::new("assets/HqGYC5G - Imgur.png")?;
+    draw::draw_board(&board, &skin, "results/board.png");
     Ok(())
 }
 
 fn tki() -> board::Board {
-    let mut board = board::Board::new(10, 20);
+    let mut board = board::Board::new(10, 10);
     board.place(&piece::Piece::O(piece::Cell { x: 0, y: 0 }, piece::Orientation::NORTH)).unwrap();
     board.place(&piece::Piece::I(piece::Cell { x: 4, y: 0 }, piece::Orientation::NORTH)).unwrap();
     board.place(&piece::Piece::Z(piece::Cell { x: 2, y: 1 }, piece::Orientation::EAST)).unwrap();
