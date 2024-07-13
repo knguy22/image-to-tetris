4b7de94385954d9c3b5d371d33de33f21c6d539b
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-13 13:46:04 -0700

    refactor the drawing

--- src/draw.rs
+++ src/draw.rs
@@ -1,63 +1,74 @@
 use crate::board::Board;
 
+use image::DynamicImage;
 use imageproc::{image, image::GenericImageView};
-use std::path;
 
-pub fn draw_board(board: &Board) {
-    let i_img = image::open("assets/section_2.png").unwrap();
-    let o_img = image::open("assets/section_3.png").unwrap();
-    let t_img = image::open("assets/section_4.png").unwrap();
-    let l_img = image::open("assets/section_5.png").unwrap();
-    let j_img = image::open("assets/section_6.png").unwrap();
-    let s_img = image::open("assets/section_7.png").unwrap();
-    let z_img = image::open("assets/section_8.png").unwrap();
+pub struct BlockSkin {
+    empty_img: image::DynamicImage,
+    i_img: image::DynamicImage,
+    o_img: image::DynamicImage,
+    t_img: image::DynamicImage,
+    l_img: image::DynamicImage,
+    j_img: image::DynamicImage,
+    s_img: image::DynamicImage,
+    z_img: image::DynamicImage,
 
-    let block_img_width = i_img.width();
-    let block_img_height = i_img.height();
+    width: u32,
+    height: u32
+}
 
-    let mut img = image::RgbaImage::new(board.width as u32 * block_img_width, board.height as u32 * block_img_height);
+impl BlockSkin {
+    pub fn new(skin_path: &str) -> Result<BlockSkin, Box<dyn std::error::Error>> {
+        let img = imageproc::image::open(skin_path)?;
+
+        const NUM_SECTIONS: usize = 9;
+        let (width, height) = img.dimensions();
+        let section_width = width / NUM_SECTIONS as u32;
+        let img_buffer = img.into_rgb8();
+
+        // split the skin into sections
+        let mut new_images: [DynamicImage; NUM_SECTIONS] = Default::default();
+        for i in 0..NUM_SECTIONS as u32 {
+            let section = image::SubImage::new(&img_buffer, i * section_width, 0, section_width, height);
+            new_images[i as usize] = section.to_image().into();
+        }
+        
+        // return the skin
+        Ok(BlockSkin {
+            empty_img: new_images[0].clone(),
+            i_img: new_images[6].clone(),
+            o_img: new_images[4].clone(),
+            t_img: new_images[8].clone(),
+            l_img: new_images[3].clone(),
+            j_img: new_images[7].clone(),
+            s_img: new_images[5].clone(),
+            z_img: new_images[2].clone(),
+            width: section_width,
+            height
+        })
+    }
+}
+
+
+pub fn draw_board(board: &Board, skin: &BlockSkin, save_path: &str) {
+    let mut img = image::RgbaImage::new(board.width as u32 * skin.width, board.height as u32 * skin.height);
 
     for y in 0..board.height {
         for x in 0..board.width {
             let block = match board.cells[y * board.width + x] {
-                'I' => &i_img,
-                'O' => &o_img,
-                'T' => &t_img,
-                'L' => &l_img,
-                'J' => &j_img,
-                'S' => &s_img,
-                'Z' => &z_img,
-                _ => { continue; }
+                'I' => &skin.i_img,
+                'O' => &skin.o_img,
+                'T' => &skin.t_img,
+                'L' => &skin.l_img,
+                'J' => &skin.j_img,
+                'S' => &skin.s_img,
+                'Z' => &skin.z_img,
+                _ => &skin.empty_img,
             };
-            image::imageops::overlay(&mut img, block, (x as u32 * block_img_width).into(), (y as u32 * block_img_height).into());
+            image::imageops::overlay(&mut img, block, (x as u32 * skin.width).into(), (y as u32 * skin.height).into());
         }
     }
     // flip the image
     img = image::imageops::flip_vertical(&img);
-    img.save("results/board.png").unwrap();
-}
-
-
-
-pub fn split_blocks(img_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let img = imageproc::image::open(img_path)?;
-
-    let (width, height) = img.dimensions();
-    let num_sections = 9;
-    let section_width = width / num_sections;
-    let img_buffer = img.into_rgb8();
-
-    for i in 0..num_sections {
-        let save_path_str = format!("assets/section_{}.png", i);
-        let save_path = path::Path::new(&save_path_str);
-        
-        let section = image::ImageBuffer::from_fn(section_width, height, |x, y| {
-            let pixel = img_buffer.get_pixel(section_width * i + x, y);
-            *pixel
-        });
-
-        section.save(save_path)?;
-    }
-
-    Ok(())
+    img.save(save_path).unwrap();
 }
