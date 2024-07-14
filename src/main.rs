0a0d6ea0310fdd1b416261f90841491f0930b283
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-14 16:54:11 -0700

    make everything cmd based

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,51 @@ mod board;
 mod draw;
 mod piece;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let source_img = image::open("sources/rick-astley-890513150.jpg")?;
-    // let source_img = image::open("sources/only_z.png")?;
+use std::path::PathBuf;
+use clap::Parser;
 
-    let config = draw::Config {
-        skin: draw::BlockSkin::new("assets/HqGYC5G - Imgur.png")?,
-        board_width: 192,
-        board_height: 108,
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    // the image to be approximated
+    #[clap(long, short, action)]
+    source_img: PathBuf,
+
+    // the output image
+    #[clap(long, short, action)]
+    output_img: PathBuf,
+
+    // board width
+    #[clap(long, short, action)]
+    width: Option<usize>,
+
+    // board height
+    #[clap(long, short, action)]
+    height: Option<usize>,
+}
+
+
+fn main() {
+    let cli = Cli::parse();
+
+    let board_width = match cli.width {
+        Some(width) => width,
+        None => 10,
+    };
+    let board_height = match cli.height {
+        Some(height) => height,
+        None => 20,
     };
 
-    let result_img = approx::approximate(&source_img, &config)?;
-    result_img.save("results/board.png")?;
+    let config = draw::Config {
+        skin: draw::BlockSkin::new("assets/HqGYC5G - Imgur.png").unwrap(),
+        board_width: board_width,
+        board_height: board_height,
+    };
 
-    Ok(())
+    let source_img = image::open(cli.source_img).unwrap();
+    let result_img = approx::approximate(&source_img, &config).unwrap();
+    result_img.save(cli.output_img).unwrap();
 }
 
 #[cfg(test)]
