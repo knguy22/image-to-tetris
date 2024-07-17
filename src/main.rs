3476f69eb8af2f2d280fbaf879e5fb175b5d811b
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-17 15:16:55 -0700

    update test

--- src/main.rs
+++ src/main.rs
@@ -43,20 +43,41 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::{fs, path};
+
     use draw::SkinnedBoard;
 
     use super::*;
 
-    #[ignore]
     #[test]
     fn test_draw_all_pieces() {
+        let width = 10;
+        let height = 20;
+        let skin_id = 0;
+        let test_dir = "test_results";
+        if !path::Path::new(&test_dir).exists() {
+            fs::create_dir(test_dir).unwrap();
+        }
+
         for orientation in piece::Orientation::all() {
             for piece in piece::Piece::all_normal(piece::Cell { x: 4, y: 4 }, orientation) {
-                let mut board = SkinnedBoard::new(10, 20);
+                let mut board = SkinnedBoard::new(width, height);
 
-                board.place(&piece, 0).unwrap();
+                // place regular piece
+                board.place(&piece, skin_id).unwrap();
+                
+                // fill the rest with black garbage
+                for y in 0..height {
+                    for x in 0..width {
+                        let cell = piece::Cell { x: x, y: y };
+                        if board.empty_at(&cell) {
+                            board.place(&piece::Piece::Black(cell), skin_id).unwrap();
+                        }
+                    }
+                }
+
                 let img = draw::draw_board(&board);
-                img.save(format!("results/{:?} {:?}.png", piece, piece.get_orientation())).unwrap();
+                img.save(format!("{}/{:?} {:?}.png", test_dir, piece, piece.get_orientation())).unwrap();
             }
         }
     }
