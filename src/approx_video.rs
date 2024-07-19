fc4c91021271741ebab4366395297380aad2c8fa
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-19 13:51:54 -0700

    refactor cli handling

--- src/approx_video.rs
+++ src/approx_video.rs
@@ -2,6 +2,7 @@ use ffmpeg_next::{codec, format, Rational, Packet, Error};
 use ffmpeg_next::frame::Video;
 use ffmpeg_next::software::scaling::{context::Context, flag::Flags};
 use ffmpeg_next::util::format::pixel::Pixel;
+use image::{DynamicImage, ImageBuffer};
 
 pub struct VideoData {
     pub width: u32,
@@ -82,66 +83,77 @@ pub fn extract_rgb_frames(file_name: &str) -> VideoData {
     }
 }
 
-pub fn save_from_rgb_frames(video: &VideoData, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut output = format::output(&file_name)?;
-    let global_header = output.format().flags().contains(format::flag::Flags::GLOBAL_HEADER);
-    let mut stream = output.add_stream(codec::Id::H264)?;
+pub fn frames_to_images(video: &VideoData) -> Vec<DynamicImage> {
+    let mut frames = Vec::new();
 
-    {
-        let mut encoder = stream.codec().encoder().video()?;
-        encoder.set_width(video.width);
-        encoder.set_height(video.height);
-        encoder.set_format(video.format);
-        encoder.set_frame_rate(Some(Rational::new(video.fps, 1)));
-        encoder.set_time_base(Rational::new(1, video.fps));
-        if global_header {
-            encoder.set_flags(codec::flag::Flags::GLOBAL_HEADER);
-        }
-    }
-
-    // open output file
-    output.write_header()?;
-
-    // Create a scaler context to convert frames to YUV format
-    let mut scaler = Context::get(
-        video.format,
-        video.width,
-        video.height,
-        Pixel::YUV420P,
-        video.width,
-        video.height,
-        Flags::BILINEAR,
-    )?;
-
-    let mut pts = 0;
-    let mut video_encoder = stream.codec().encoder().video()?;
     for rgb_data in video.frames.iter() {
-        let mut rgb_frame = Video::empty();
-        rgb_frame.set_format(video.format);
-        rgb_frame.set_width(video.width);
-        rgb_frame.set_height(video.height);
-        rgb_frame.data_mut(0).copy_from_slice(&rgb_data);
-
-        let mut yuv_frame = Video::empty();
-        yuv_frame.set_format(Pixel::YUV410P);
-        yuv_frame.set_width(video.width);
-        yuv_frame.set_height(video.height);
-
-        scaler.run(&rgb_frame, &mut yuv_frame)?;
-        yuv_frame.set_pts(Some(pts));
-        pts += 1;
-
-        video_encoder.send_frame(&yuv_frame)?;
-
-        // flush encoder
-        video_encoder.send_eof()?;
-        let mut encoded = Packet::empty();
-        while video_encoder.receive_packet(&mut encoded).is_ok() {
-            encoded.set_stream(0);
-            output.write_header(&encoded)?;
-        }
+        let frame: ImageBuffer<image::Rgba<u8>, _> = image::ImageBuffer::from_raw(video.width, video.height, rgb_data.to_owned()).unwrap();
+        frames.push(DynamicImage::from(frame));
     }
 
-
-    Ok(())
+    frames
 }
+
+// pub fn save_from_rgb_frames(video: &VideoData, file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+//     let mut output = format::output(&file_name)?;
+//     let global_header = output.format().flags().contains(format::flag::Flags::GLOBAL_HEADER);
+//     let mut stream = output.add_stream(codec::Id::H264)?;
+
+//     {
+//         let mut encoder = stream.codec().encoder().video()?;
+//         encoder.set_width(video.width);
+//         encoder.set_height(video.height);
+//         encoder.set_format(video.format);
+//         encoder.set_frame_rate(Some(Rational::new(video.fps, 1)));
+//         encoder.set_time_base(Rational::new(1, video.fps));
+//         if global_header {
+//             encoder.set_flags(codec::flag::Flags::GLOBAL_HEADER);
+//         }
+//     }
+
+//     // open output file
+//     output.write_header()?;
+
+//     // Create a scaler context to convert frames to YUV format
+//     let mut scaler = Context::get(
+//         video.format,
+//         video.width,
+//         video.height,
+//         Pixel::YUV420P,
+//         video.width,
+//         video.height,
+//         Flags::BILINEAR,
+//     )?;
+
+//     let mut pts = 0;
+//     let mut video_encoder = stream.codec().encoder().video()?;
+//     for rgb_data in video.frames.iter() {
+//         let mut rgb_frame = Video::empty();
+//         rgb_frame.set_format(video.format);
+//         rgb_frame.set_width(video.width);
+//         rgb_frame.set_height(video.height);
+//         rgb_frame.data_mut(0).copy_from_slice(&rgb_data);
+
+//         let mut yuv_frame = Video::empty();
+//         yuv_frame.set_format(Pixel::YUV410P);
+//         yuv_frame.set_width(video.width);
+//         yuv_frame.set_height(video.height);
+
+//         scaler.run(&rgb_frame, &mut yuv_frame)?;
+//         yuv_frame.set_pts(Some(pts));
+//         pts += 1;
+
+//         video_encoder.send_frame(&yuv_frame)?;
+
+//         // flush encoder
+//         video_encoder.send_eof()?;
+//         let mut encoded = Packet::empty();
+//         while video_encoder.receive_packet(&mut encoded).is_ok() {
+//             encoded.set_stream(0);
+//             output.write_header(&encoded)?;
+//         }
+//     }
+
+
+//     Ok(())
+// }
