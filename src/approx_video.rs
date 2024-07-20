923e1ffb0b032679afb0736eecf1ddc3918ebfaa
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-19 17:55:07 -0700

    crashing on YUV from RGB

--- src/approx_video.rs
+++ src/approx_video.rs
@@ -6,6 +6,8 @@ use ffmpeg_next::{codec, format, Rational, Packet, Error};
 use ffmpeg_next::frame::Video;
 use ffmpeg_next::software::scaling::{context::Context, flag::Flags};
 use ffmpeg_next::util::format::pixel::Pixel;
+use ffmpeg_next::util::picture;
+use ffmpeg_next::codec::picture::Picture;
 use image::{DynamicImage, ImageBuffer};
 
 pub fn run(source: &PathBuf, output: &PathBuf, board_width: usize, board_height: usize) {
@@ -20,8 +22,8 @@ pub fn run(source: &PathBuf, output: &PathBuf, board_width: usize, board_height:
     };
 
     for (i, frame) in input.enumerate() {
-        let approx_img = approx::approximate(&mut frame_to_image(frame, &meta_data), &config).unwrap();
-        output.send_frame(&approx_img);
+        let approx_img = approx::approximate(&mut frame_to_image(frame, &meta_data), &config).expect("failed to approximate image");
+        output.send_frame(&approx_img, &meta_data);
 
         if (i + 1) % 10 == 0 {
             println!("Approximated {} frames", i + 1);
@@ -59,8 +61,8 @@ struct VideoOutput {
 
 impl VideoInput {
     fn new(file_name: &PathBuf) -> (VideoMetaData, VideoInput) {
-        let source = format::input(file_name).unwrap();
-        let input = source.streams().best(ffmpeg_next::media::Type::Video).unwrap();
+        let source = format::input(file_name).expect("failed to open input file");
+        let input = source.streams().best(ffmpeg_next::media::Type::Video).expect("failed to find video stream");
 
         // video metadata
         let video_stream_index = input.index();
@@ -69,7 +71,7 @@ impl VideoInput {
         let format = Pixel::RGB24;
 
         // setup the decoder
-        let decoder = input.codec().decoder().video().unwrap();
+        let decoder = input.codec().decoder().video().expect("failed to create decoder");
         let scaler = Context::get(
             decoder.format(),
             decoder.width(),
@@ -78,7 +80,7 @@ impl VideoInput {
             decoder.width(),
             decoder.height(),
             Flags::BILINEAR,
-        ).unwrap();
+        ).expect("failed to create scaler");
 
         (VideoMetaData {
             width: decoder.width(),
@@ -103,14 +105,14 @@ impl Iterator for VideoInput {
         while let Some((stream, packet)) = self.source.packets().next() {
             if stream.index() == self.video_stream_index {
                 // send the video packet to the codec
-                self.decoder.send_packet(&packet).unwrap();
+                self.decoder.send_packet(&packet).expect("failed to send packet to decoder");
                 
                 // then attempt to decode and collect that packet into a frame
                 let mut frame = Video::empty();
                 if self.decoder.receive_frame(&mut frame).is_ok() {
                     // then scale the frame, which is now ready to push
                     let mut rgb_frame = Video::empty();
-                    self.scaler.run(&frame, &mut rgb_frame).unwrap();
+                    self.scaler.run(&frame, &mut rgb_frame).expect("failed to scale frame");
                     let data = rgb_frame.data(0).to_vec();
                     return Some(data)
                 }
@@ -121,7 +123,7 @@ impl Iterator for VideoInput {
         let mut frame = Video::empty();
         while self.decoder.receive_frame(&mut frame).is_ok() {
             let mut rgb_frame = Video::empty();
-            self.scaler.run(&frame, &mut rgb_frame).unwrap();
+            self.scaler.run(&frame, &mut rgb_frame).expect("failed to scale frame");
 
             // Collect the frame data
             let data = rgb_frame.data(0).to_vec();
@@ -172,14 +174,32 @@ impl VideoOutput {
         }
     }
 
-    fn send_frame(&mut self, image: &DynamicImage) {
-        let mut rgb_frame = Video::empty();
-        let data = image.to_rgb8().into_vec();
-        rgb_frame.set_format(Pixel::RGB24);
-        rgb_frame.set_width(image.width());
-        rgb_frame.set_height(image.height());
-        rgb_frame.data_mut(0).copy_from_slice(&data);
-        self.encoder.send_frame(&rgb_frame).unwrap();
+    fn send_frame(&mut self, image: &DynamicImage, video: &VideoMetaData) {
+        // Convert image to RGB24 format
+        let rgb_image = image.to_rgb8();
+        let width = rgb_image.width();
+        let height = rgb_image.height();
+        let data = rgb_image.into_raw();
+
+        // Create a frame for the RGB image; make sure to check for buffer bounds and copy
+        let mut rgb_frame = Video::new(Pixel::RGB24, width, height);
+        let buffer = rgb_frame.data_mut(0);
+        let buffer_len = buffer.len();
+        assert!(buffer.len() > data.len(), "RGB frame buffer too small");
+        let copy_len = data.len().min(buffer_len);
+        buffer[..copy_len].copy_from_slice(&data[..copy_len]);
+
+        // Create a frame for the YUV420P output
+        let mut yuv_frame = Video::new(Pixel::YUV420P, video.height, video.width);
+        println!("{}x{}", video.width, video.height);
+        println!("{}x{}", rgb_frame.width(), rgb_frame.height());
+        println!("{}x{}", yuv_frame.width(), yuv_frame.height());
+
+        // Convert RGB24 to YUV420P
+        self.scaler.run(&rgb_frame, &mut yuv_frame).expect("failed to convert RGB24 to YUV420P");
+
+        // Send the YUV420P frame to the encoder
+        self.encoder.send_frame(&yuv_frame).expect("failed to send frame to encoder");
     }
 }
 
