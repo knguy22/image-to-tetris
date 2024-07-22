a54cb5a2de372447b03e0f3bca3b06724b8bfd9f
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-21 20:50:01 -0700

    modify approx video to use ffmpeg from command line instead of lib

--- src/approx_video.rs
+++ src/approx_video.rs
@@ -1,129 +1,125 @@
 use crate::{approx, draw};
 
+use std::fs;
 use std::path::PathBuf;
+use std::process::Command;
 
-use ffmpeg_next::{format, Rational};
-use ffmpeg_next::frame::Video;
-use ffmpeg_next::software::scaling::{context::Context, flag::Flags};
-use ffmpeg_next::util::format::pixel::Pixel;
-use image::{DynamicImage, ImageBuffer};
+use ffmpeg_next::format;
 
 pub fn run(source: &PathBuf, output: &PathBuf, board_width: usize, board_height: usize) {
+    const SOURCE_IMG_DIR: &str = "video_sources";
+    const APPROX_IMG_DIR: &str = "video_approx";
+    const AUDIO_PATH: &str = "video_approx/audio.wav";
+
     ffmpeg_next::init().expect("failed to initialize ffmpeg");
-    let (meta_data, input) = VideoInput::new(source);
-    let config = draw::Config {
+    let source_path = source.to_str().expect("failed to convert source path to string");
+    let output_path = output.to_str().expect("failed to convert output path to string");
+
+    // check for the prerequisite directories to exist
+    if !PathBuf::from(SOURCE_IMG_DIR).exists() {
+        fs::create_dir(SOURCE_IMG_DIR).expect("failed to create video_sources directory");
+    }
+    if !PathBuf::from(APPROX_IMG_DIR).exists() {
+        fs::create_dir(APPROX_IMG_DIR).expect("failed to create video_sources directory");
+    }
+
+    // make sure the directories are empty; crash if not
+    if fs::read_dir(SOURCE_IMG_DIR).unwrap().count() > 0 {
+        panic!("video_sources directory is not empty");
+    }
+    if fs::read_dir(APPROX_IMG_DIR).unwrap().count() > 0 {
+        panic!("video_approx directory is not empty");
+    }
+
+    // load config
+    let video_config = VideoConfig::new(source).expect("failed to load video config");
+    let draw_config = draw::Config {
         board_width: board_width,
         board_height: board_height,
     };
-    println!("Loaded {}x{} resolution video", meta_data.width, meta_data.height);
+    println!("Approximating video with {}x{} dimensions using {}x{} board", video_config.width, video_config.height, board_width, board_height);
+    println!("Using {} fps", video_config.fps);
 
-    for (i, frame) in input.enumerate() {
-        let mut target_img = frame_to_image(frame, &meta_data);
-        let approx_img = approx::approximate(&mut target_img, &config).expect("failed to approximate image");
-        approx_img.save(format!("tmp_video/{}.png", i)).expect("failed to save image");
-
-        if (i + 1) % 10 == 0 {
-            println!("Approximated {} frames", i + 1);
-        }
+    // use ffmpeg to generate a directory full of images
+    let gen_image_command = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(source_path)
+        .arg("-vf")
+        .arg(format!("fps={}", video_config.fps))
+        .arg("-start_number")
+        .arg("0")
+        .arg(format!("{}/%d.png", SOURCE_IMG_DIR))
+        .output()
+        .expect("failed to create source images");
+    if !gen_image_command.status.success() {
+        println!("ffmpeg error: {:?}", gen_image_command);
+        panic!("failed to generate source images");
     }
-}
 
-type RawFrame = Vec<u8>;
+    // use ffmpeg to generate the audio file
+    let gen_audio_command = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(source_path)
+        .arg(AUDIO_PATH)
+        .output()
+        .expect("failed to create audio file");
+    if !gen_audio_command.status.success() {
+        panic!("failed to generate source images");
+    }
+
+    // approximate the source images
+    for file in fs::read_dir(SOURCE_IMG_DIR).expect("failed to read source images directory") {
+        let source_path = file.expect("failed to read source image").path();
+        let source_path_without_dir = source_path.file_name().expect("failed to get source image path without directory");
+        let approx_path = format!("{}/{}", APPROX_IMG_DIR, source_path_without_dir.to_str().expect("failed to convert source image path to string"));
+
+        let mut source_img = image::open(source_path).expect("failed to load source image");
+        let approx_img = approx::approximate(&mut source_img, &draw_config).expect("failed to approximate image");
+        approx_img.save(approx_path).expect("failed to save approx image");
+    }
+
+    // combine the approximated images and audio for a final video
+    let _combine_command = Command::new("ffmpeg")
+        .arg("-framerate")
+        .arg(format!("{}", video_config.fps))
+        .arg("-i")
+        .arg(format!("{}/%d.png", APPROX_IMG_DIR))
+        .arg("-i")
+        .arg(AUDIO_PATH)
+        .arg("-c:v")
+        .arg("libx264")
+        .arg("-c:a")
+        .arg("aac")
+        .arg("-shortest")
+        .arg(output_path)
+        .output()
+        .expect("failed to combine images and audio");
+
+    // clean up the directories
+    fs::remove_dir_all(SOURCE_IMG_DIR).expect("failed to remove source images directory");
+    fs::remove_dir_all(APPROX_IMG_DIR).expect("failed to remove approximated images directory");
+}
 
 // contains important video metadata
 #[derive(Debug, Clone, Copy)]
-struct VideoMetaData {
+struct VideoConfig {
     width: u32,
     height: u32,
-    fps: Rational,
-    time_base: Rational,
-    format: Pixel,
+    fps: i32,
 }
 
-// streams from the input file
-struct VideoInput {
-    source: ffmpeg_next::format::context::Input,
-    video_stream_index: usize,
-    decoder: ffmpeg_next::codec::decoder::video::Video,
-    scaler: Context,
-}
-
-impl VideoInput {
-    fn new(file_name: &PathBuf) -> (VideoMetaData, VideoInput) {
-        let source = format::input(file_name).expect("failed to open input file");
-        let input = source.streams().best(ffmpeg_next::media::Type::Video).expect("failed to find video stream");
-
-        // video metadata
-        let video_stream_index = input.index();
+impl VideoConfig {
+    // loads video metadata
+    fn new(path: &PathBuf) -> Result<VideoConfig, Box<dyn std::error::Error>> {
+        let source = format::input(path)?;
+        let input = source.streams().best(ffmpeg_next::media::Type::Video).ok_or("failed to find video stream")?;
         let fps = input.avg_frame_rate();
-        let time_base = input.time_base();
-        let format = Pixel::RGB24;
-
-        // setup the decoder
         let decoder = input.codec().decoder().video().expect("failed to create decoder");
-        let scaler = Context::get(
-            decoder.format(),
-            decoder.width(),
-            decoder.height(),
-            format,
-            decoder.width(),
-            decoder.height(),
-            Flags::BILINEAR,
-        ).expect("failed to create scaler");
 
-        (VideoMetaData {
+        Ok(VideoConfig {
             width: decoder.width(),
             height: decoder.height(),
-            fps: fps,
-            time_base: time_base,
-            format: format,
-        },
-        VideoInput {
-            source: source,
-            video_stream_index: video_stream_index,
-            decoder: decoder,
-            scaler: scaler,
+            fps: fps.numerator() / fps.denominator(),
         })
     }
-}
-
-impl Iterator for VideoInput {
-    type Item = RawFrame;
-    fn next(&mut self) -> Option<Self::Item> {
-        // Loop through the packets in the video file
-        while let Some((stream, packet)) = self.source.packets().next() {
-            if stream.index() == self.video_stream_index {
-                // send the video packet to the codec
-                self.decoder.send_packet(&packet).expect("failed to send packet to decoder");
-                
-                // then attempt to decode and collect that packet into a frame
-                let mut frame = Video::empty();
-                if self.decoder.receive_frame(&mut frame).is_ok() {
-                    // then scale the frame, which is now ready to push
-                    let mut rgb_frame = Video::empty();
-                    self.scaler.run(&frame, &mut rgb_frame).expect("failed to scale frame");
-                    let data = rgb_frame.data(0).to_vec();
-                    return Some(data)
-                }
-            }
-        }
-
-        // Flush the decoder to get frames from remaining packets
-        let mut frame = Video::empty();
-        while self.decoder.receive_frame(&mut frame).is_ok() {
-            let mut rgb_frame = Video::empty();
-            self.scaler.run(&frame, &mut rgb_frame).expect("failed to scale frame");
-
-            // Collect the frame data
-            let data = rgb_frame.data(0).to_vec();
-            return Some(data)
-        }
-
-        None
-    }
-}
-
-fn frame_to_image(frame: RawFrame, video: &VideoMetaData) -> DynamicImage {
-    let buffer: ImageBuffer<image::Rgb<u8>, _> = image::ImageBuffer::from_raw(video.width, video.height, frame.to_owned()).expect("failed to create image from raw data");
-    DynamicImage::from(buffer)
 }
