8ff4d0fc115772e7631f318fe642bf6338c63baa
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-23 00:11:16 -0700

    premature optimizations about skins

--- src/main.rs
+++ src/main.rs
@@ -25,36 +25,42 @@ fn main() {
     };
     println!("Prioritizing tetrominos: {}", cli.prioritize_tetrominos);
 
+    // a global skins will be copied by each thread to prevent needing IO to recreate skins for each thread
+    let skins = draw::create_skins();
+
     match cli.command {
         cli::Commands::Integration {board_width} => {
             let config = approx_image::Config {
                 board_width: board_width.unwrap_or(100),
                 board_height: 0, // height doesn't matter here since it will be auto-scaled
-                prioritize_tetrominos
+                prioritize_tetrominos,
+                skins: &skins,
             };
             integration_test::run("sources", &config).unwrap()
         },
         cli::Commands::ApproxImage { source, output, board_width, board_height } => {
-            run_approx_image(&source, &output, board_width, board_height, prioritize_tetrominos)
+            let config = approx_image::Config {
+                board_width,
+                board_height,
+                prioritize_tetrominos,
+                skins: &skins,
+            };
+            run_approx_image(&source, &output, &config)
         }
         cli::Commands::ApproxVideo { source, output, board_width, board_height } => {
             let config = approx_image::Config {
                 board_width,
                 board_height,
-                prioritize_tetrominos
+                prioritize_tetrominos,
+                skins: &skins,
             };
             approx_video::run(&source, &output, &config)
         }
     }
 }
 
-fn run_approx_image(source: &PathBuf, output: &PathBuf, board_width: usize, board_height: usize, prioritize_tetrominos: approx_image::PrioritizeColor) {
+fn run_approx_image(source: &PathBuf, output: &PathBuf, config: &approx_image::Config) {
     println!("Approximating an image: {}", source.display());
-    let config = approx_image::Config {
-        board_width: board_width,
-        board_height: board_height,
-        prioritize_tetrominos
-    };
 
     let mut source_img = image::open(source).unwrap();
     println!("Loaded {}x{} image", source_img.width(), source_img.height());
@@ -84,6 +90,7 @@ mod tests {
             fs::create_dir(test_dir).unwrap();
         }
 
+        let skins = draw::create_skins();
         let all_piece_types: Vec<_> = piece::Orientation::all()
             .into_iter()
             .flat_map(|o| piece::Piece::all_normal(piece::Cell { x: 4, y: 4 }, o))
@@ -92,7 +99,7 @@ mod tests {
         all_piece_types
             .into_par_iter()
             .for_each(|piece| {
-                let mut board = SkinnedBoard::new(width, height);
+                let mut board = SkinnedBoard::new(width, height, &skins);
 
                 // place regular piece
                 board.place(&piece, skin_id).unwrap();
