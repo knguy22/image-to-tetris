0ce55369829878c28439f1f019c062b16f7aa769
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-29 15:17:22 -0700

    gameplan

--- src/approx_audio.rs
+++ src/approx_audio.rs
@@ -1,5 +1,14 @@
 use std::path::PathBuf;
 
+use fundsp::prelude::*;
+
+// Todo:
+// Be able to extract audio and metadata into a struct (do this for both input and tetris sample clips)
+// Represent the tetris clips inside a single struct
+// Split the source into multiple chunks somehow (note detection?)
+// approximate each individual chunk
+// combine the chunks into a single audio file
+
 pub fn run(source: &PathBuf, output: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    todo!()
+    todo!();
 }
