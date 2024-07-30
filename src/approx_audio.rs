a902898895fddb5006d5c3668453654072bbaa69
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-07-30 15:01:39 -0700

    successfully chunkify input audio (simple method)

--- src/approx_audio.rs
+++ src/approx_audio.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
-use std::sync::Arc;
 use std::fmt;
 
 use fundsp::prelude::*;
+use itertools::Itertools;
 
 // Todo:
 // Be able to extract audio and metadata into a struct (do this for both input and tetris sample clips)
@@ -21,14 +21,19 @@ struct InputAudioClip {
     chunks: Vec<AudioClip>,
 }
 
+// the fundamental structure of an audio clip in this project
 struct AudioClip {
-    samples: Vec<Vec<f32>>,
+    samples: Vec<Sample>,
     file_name: String,
     sample_rate: f64,
+    duration: f64,
     max_amplitude: f32,
     num_channels: usize,
     num_samples: usize,
 }
+
+// contains amplitudes for each channel at a certain timestamp
+type Sample = Vec<f32>;
 
 pub fn run(source: &PathBuf, output: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     let clip = AudioClip::new(source)?;
@@ -53,9 +58,27 @@ impl TetrisClips {
 }
 
 impl InputAudioClip {
-    pub fn new(source: &PathBuf) -> Result<InputAudioClip, Box<dyn std::error::Error>> {
+    pub fn new(source: &PathBuf, max_clip_duration: f64) -> Result<InputAudioClip, Box<dyn std::error::Error>> {
         let clip = AudioClip::new(source)?;
-        todo!()
+
+        // split the original video into parseable chunks
+        let chunk_num_samples = (max_clip_duration * clip.sample_rate) as usize;
+        let mut chunks = Vec::new();
+        for chunk in clip.samples.chunks(chunk_num_samples) {
+            chunks.push(
+                AudioClip {
+                    samples: chunk.to_vec(),
+                    file_name: clip.file_name.clone(),
+                    sample_rate: clip.sample_rate,
+                    duration: max_clip_duration,
+                    max_amplitude: clip.max_amplitude,
+                    num_channels: clip.num_channels,
+                    num_samples: chunk_num_samples,
+                }
+            )
+        }
+
+        Ok(InputAudioClip{chunks})
     }
 }
 
@@ -63,21 +86,24 @@ impl AudioClip {
     pub fn new(source: &PathBuf) -> Result<AudioClip, Box<dyn std::error::Error>> {
         let wave = Wave::load(source)?;
         let sample_rate = wave.sample_rate();
+        let duration = wave.duration();
         let max_amplitude = wave.amplitude();
         let num_channels = wave.channels();
-        let num_samples: usize = (wave.duration() * wave.sample_rate()) as usize;
-        let mut samples: Vec<Vec<f32>> = Vec::new();
-        for channel in 0..num_channels {
-            let mut channel_samples = Vec::new();
-            for index in 0..num_samples {
-                channel_samples.push(wave.at(channel, index));
+        let num_samples: usize = (duration * sample_rate) as usize;
+        let mut samples: Vec<Sample> = Vec::new();
+
+        for index in 0..num_samples {
+            let mut sample = Sample::new();
+            for channel in 0..num_channels {
+                sample.push(wave.at(channel, index));
             }
-            samples.push(channel_samples);
+            samples.push(sample);
         }
 
         Ok(AudioClip {
             samples,
             file_name: source.to_str().unwrap().to_string(),
+            duration,
             sample_rate,
             max_amplitude,
             num_channels,
@@ -107,6 +133,18 @@ mod tests {
     #[test]
     fn test_create_audio_clip() {
         let source = path::PathBuf::from("test_sources/a6.mp3");
-        let _ = AudioClip::new(&source).expect("failed to create audio clip");
+        let clip = AudioClip::new(&source).expect("failed to create audio clip");
+
+        assert_ne!(clip.num_channels, 0);
+        assert_ne!(clip.num_samples, 0);
+        assert_ne!(clip.samples.len(), 0);
+    }
+
+    #[test]
+    fn test_split_input_audio_clip() {
+        let source = path::PathBuf::from("test_sources/a6.mp3");
+        let clip = InputAudioClip::new(&source, 0.2).expect("failed to create audio clip");
+        
+        assert_eq!(clip.chunks.len(), 15);
     }
 }
