0b499d7b953acc8901367722a32493ab64d8d7c0
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-08-02 16:01:19 -0700

    only use a single channel for fft; this makes processing easier

--- src/approx_audio/fft.rs
+++ src/approx_audio/fft.rs
@@ -4,11 +4,10 @@ use std::path::PathBuf;
 use itertools::Itertools;
 use rustfft::{FftPlanner, num_complex::Complex};
 
-pub type FFTChannel = Vec<FFTSample>;
 pub type FFTSample = Complex<Sample>;
 
 pub struct FFTResult {
-    pub channels: Vec<FFTChannel>,
+    pub samples: Vec<FFTSample>,
     pub frequency_resolution: f64,
 }
 
@@ -16,20 +15,25 @@ impl AudioClip {
     pub fn fft(&self) -> FFTResult {
         let mut planner = FftPlanner::<Sample>::new();
         let fft = planner.plan_fft_forward(self.num_samples);
-        let mut fft_final: Vec<FFTChannel> = Vec::new();
 
-        // perform the FFT on each channel
-        for channel in self.channels.iter() {
-            let mut buffer = channel
-                .iter()
-                .map(|f| Complex::new(*f, 0.0))
-                .collect_vec();
-            fft.process(&mut buffer);
-            fft_final.push(buffer);
-        }
+        // average out all the samples across channels for each sample index
+        // then turn the average into a complex number
+        let mut average_samples = (0..self.num_samples)
+            .map(|i| {
+                self.channels
+                    .iter()
+                    .map(|c| c[i])
+                    .reduce(|a, b| a + b)
+                    .unwrap() / self.channels.len() as Sample
+            })
+            .map(|f| Complex::new(f, 0.0))
+            .collect_vec();
+
+        // perform the FFT using the averaged samples
+        fft.process(&mut average_samples);
 
         FFTResult {
-            channels: fft_final,
+            samples: average_samples,
             frequency_resolution: self.sample_rate / self.num_samples as f64,
         }
     }
@@ -40,12 +44,10 @@ impl FFTResult {
     pub fn dump(&self) -> Result<(), Box<dyn std::error::Error>> {
         let output = PathBuf::from("python/fft.csv");
         let mut wtr = csv::Writer::from_path(output)?;
-        wtr.write_record(&["channel", "frequency", "norm"])?;
-        for (channel_idx, channel) in self.channels.iter().enumerate() {
-            for (i, sample) in channel.iter().enumerate() {
-                let frequency = self.frequency_resolution * i as f64;
-                wtr.write_record(&[channel_idx.to_string(), frequency.to_string(), sample.norm().to_string()])?;
-            }
+        wtr.write_record(&["frequency", "norm"])?;
+        for (i, sample) in self.samples.iter().enumerate() {
+            let frequency = self.frequency_resolution * i as f64;
+            wtr.write_record(&[frequency.to_string(), sample.norm().to_string()])?;
         }
 
         Ok(())
@@ -60,3 +62,24 @@ impl fmt::Debug for FFTResult {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path;
+
+    #[test]
+    fn test_fft() {
+        let source = path::PathBuf::from("test_audio_clips/a6.mp3");
+        let clip = AudioClip::new(&source).expect("failed to create audio clip");
+        let fft = clip.fft();
+        assert_ne!(fft.samples.len(), 0);
+    }
+
+    #[test]
+    fn test_dump() {
+        let source = path::PathBuf::from("test_audio_clips/a6.mp3");
+        let clip = AudioClip::new(&source).expect("failed to create audio clip");
+        let fft = clip.fft();
+        fft.dump().unwrap();
+    }
+}
