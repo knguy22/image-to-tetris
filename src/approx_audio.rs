ead0110a6b820b4bef62d05eed37318fba6ee730
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-08-01 19:17:55 -0700

    remove todo

--- src/approx_audio.rs
+++ src/approx_audio.rs
@@ -9,13 +9,6 @@ use std::cmp;
 
 use itertools::Itertools;
 
-// Todo:
-// Be able to extract audio and metadata into a struct (do this for both input and tetris sample clips)
-// Represent the tetris clips inside a single struct
-// Split the source into multiple chunks somehow (note detection?)
-// approximate each individual chunk
-// combine the chunks into a single audio file
-
 #[derive(Debug)]
 struct TetrisClips {
     clips: Vec<AudioClip>
