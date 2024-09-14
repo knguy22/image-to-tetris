52803ef9a04483bdfe7d829405bd096bde02f3e4
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-09-13 22:35:47 -0700

    Try onset detection using phase (#20)
    * refactor diffs into multiple functions; this allows us to experiment with phase more easily
    
    * incomplete onset detection using phase:
    
    Final MSE: 0.042124304333587916
    Final Dot: 0.00008522520851272721
    
    Also 831 sections vs 593
    
    * try principal argument
    
    Final MSE: 0.04242371445931301
    Final Dot: -0.00006253327639627325
    
    * filter non negs, restore constants to match spectrum
    
    Final MSE: 0.04230389000851317
    Final Dot: -0.00006935980335579393
    
    519 sections
    
    * best phase so far:
    
    Final MSE: 0.041976767688508984
    Final Dot: 0.00007706243109389812
    
    * add test
    
    Overall, spectrum still performed better.

--- src/approx_audio/onset_detect.rs
+++ src/approx_audio/onset_detect.rs
@@ -1,7 +1,8 @@
 use itertools::Itertools;
 
+use crate::approx_audio::audio_clip::Channel;
 use super::audio_clip::{AudioClip, Sample};
-use super::fft::{get_norms, FFTNorms, STFTNorms};
+use super::fft::{get_norms, FFTNorms, FFTResult, FFTSample, STFTNorms};
 use super::windowing::{hanning_window, rectangle_window};
 
 use anyhow::Result;
@@ -9,23 +10,20 @@ use anyhow::Result;
 /// a vector of sample indices that contain properly detected onsets
 pub type Onsets = Vec<usize>;
 
-/// the difference between two fft timestamps, expressed as a single sample
-type FFTDiff = Sample;
-
-/// multiple `FFTDiffs` over different timestamps
-type STFTDiffs = Vec<FFTDiff>;
-
 impl AudioClip {
     pub fn split_by_onsets(&self) -> Vec<AudioClip> {
-        let mut onsets = self.detect_onsets();
+        let mut onsets = self.detect_onsets_spectrum();
 
         // we need to include the beginning and the end in the onsets to include the whole clip
-        if onsets[0] != 0 {
+        if onsets[0] > 0 {
             onsets.insert(0, 0);
         }
-        if onsets[onsets.len() - 1] != self.num_samples {
+        assert!(onsets.first().unwrap() == &0);
+
+        if onsets[onsets.len() - 1] < self.num_samples {
             onsets.push(self.num_samples);
         }
+        assert!(onsets.last().unwrap() == &self.num_samples);
 
         onsets
             .iter()
@@ -39,8 +37,9 @@ impl AudioClip {
 
     // gives a vector of sample indices that are onsets
     // this currently uses spectrum onset detection
-    #[allow(clippy::cast_precision_loss, clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-    fn detect_onsets(&self) -> Onsets {
+    // method sourced from: https://www.audiolabs-erlangen.de/resources/MIR/FMP/C6/C6S1_NoveltySpectral.html
+    #[allow(clippy::cast_precision_loss, clippy::cast_sign_loss, clippy::cast_possible_truncation, unused)]
+    fn detect_onsets_spectrum(&self) -> Onsets {
         // perform short time fourier transform
         let window_size = 2048;
         let hop_size = window_size / 4;
@@ -53,18 +52,20 @@ impl AudioClip {
         let stft = apply_gamma_log(&stft, 100.0);
 
         // take the derivative
-        let mut diffs = find_diffs(&stft);
+        let diffs = find_diffs(&stft);
+        let diffs = filter_non_negs_diffs(&diffs);
+        let mut collapsed_diffs = collapse_diffs(&diffs);
 
         // use local averages to find extraordinary diffs
         let window_sec = 0.1;
         let window_size = (window_sec * self.sample_rate as Sample / hop_size as Sample).ceil() as usize;
-        let local_avg_diffs = find_local_avgs(&diffs, window_size);
-        for (diff, local_avg_diff) in diffs.iter_mut().zip(local_avg_diffs.iter()) {
+        let local_avg_diffs = find_local_avgs(&collapsed_diffs, window_size);
+        for (diff, local_avg_diff) in collapsed_diffs.iter_mut().zip(local_avg_diffs.iter()) {
             *diff = Sample::max(*diff - local_avg_diff, 0.0);
         }
 
         // normalize the diffs so we can use them for onset detection
-        let diffs = normalize_diffs(&diffs);
+        let diffs = normalize_diffs(&collapsed_diffs);
 
         // perform onset detection using the derivative
         // onsets will typically have non-zero derivative values
@@ -84,6 +85,125 @@ impl AudioClip {
 
         onsets
     }
+
+    /// method sourced from here: https://www.audiolabs-erlangen.de/resources/MIR/FMP/C6/C6S1_NoveltyPhase.html
+    #[allow(clippy::cast_precision_loss, clippy::cast_sign_loss, clippy::cast_possible_truncation, unused)]
+    pub fn detect_onsets_phase(&self) -> Onsets {
+        // perform short time fourier transform
+        let window_size = 2048;
+        let hop_size = window_size / 4;
+        let stft = self.stft(window_size, hop_size, hanning_window);
+
+        // obtain the phase
+        let phase = find_phase_stft(&stft);
+
+        // take two derivatives
+        let diff_1 = principal_argument(&find_diffs(&phase));
+        let diff_2 = principal_argument(&find_diffs(&diff_1));
+        let mut collapsed_diffs = collapse_diffs(&filter_non_negs_diffs(&diff_2));
+
+        // use local averages to find extraordinary diffs
+        let window_sec = 0.1;
+        let window_size = (window_sec * self.sample_rate as Sample / hop_size as Sample).ceil() as usize;
+        let local_avg_diffs = find_local_avgs(&collapsed_diffs, window_size);
+        for (diff, local_avg_diff) in collapsed_diffs.iter_mut().zip(local_avg_diffs.iter()) {
+            *diff = Sample::max(*diff - local_avg_diff, 0.0);
+        }
+
+        // normalize the diffs so we can use them for onset detection
+        let diffs = normalize_diffs(&collapsed_diffs);
+
+        // perform onset detection using the derivative
+        // onsets will typically have non-zero derivative values
+        let mut onsets = Vec::new();
+        let index_iter = (0..self.num_samples).step_by(hop_size);
+        let mut last_onset = None;
+        for (&diff, index) in diffs.iter().zip_eq(index_iter) {
+            // only push onset once the diff is non-zero to a certain degree
+            if last_onset.is_none() && diff > 0.175 {
+                onsets.push(index);
+                last_onset = Some(index);
+            }
+            else if index - last_onset.unwrap_or(0) > (0.1 * self.sample_rate) as usize {
+                last_onset = None;
+            }
+        }
+
+        onsets
+    }
+}
+
+fn find_phase_stft(stft: &[FFTResult]) -> STFTNorms {
+    fn find_phase_channel(channel: &[FFTSample]) -> Channel {
+        channel.iter().map(|&sample| sample.to_polar().1 / (2.0 * std::f32::consts::PI)).collect_vec()
+    }
+
+    fn find_phase_fft(fft_result: &FFTResult) -> FFTNorms {
+        fft_result
+            .channels
+            .iter()
+            .map(|channel| find_phase_channel(channel))
+            .collect_vec()
+    }
+
+    stft
+        .iter()
+        .map(find_phase_fft)
+        .collect_vec()
+}
+
+/// this is mostly equivalent to finding the derivative of the stft
+fn find_diffs(stft: &STFTNorms) -> STFTNorms {
+    fn delta_channel(curr: &Channel, next: &Channel) -> Channel {
+        curr.iter().zip_eq(next.iter())
+            .map(|(&curr, &next)| next - curr)
+            .collect_vec()
+    }
+
+    fn delta_fft_norm(curr: &FFTNorms, next: &FFTNorms) -> Vec<Channel> {
+        // each norm contains a vector of channel norms
+        // we will compare channel to channel, norm to norm
+        curr.iter().zip_eq(next.iter())
+            .map(|(curr_channel, next_channel)| delta_channel(curr_channel, next_channel))
+            .collect_vec()
+    }
+
+    let mut diffs = stft
+        .iter()
+        .tuple_windows()
+        .map(|(curr, next)| delta_fft_norm(curr, next))
+        .collect_vec();
+
+    // the first differential is 0 since there is no preceding sample
+    let num_channels = stft[0].len();
+    let num_bins = stft[0][0].len();
+    diffs.insert(0, vec![vec![0.0; num_bins]; num_channels]);
+
+    diffs
+}
+
+fn filter_non_negs_diffs(stft: &STFTNorms) -> STFTNorms {
+    stft
+        .iter()
+        .map(|fft_norm| fft_norm
+            .iter()
+            .map(|channel| channel
+                .iter()
+                .map(|&sample| Sample::max(sample, 0.0))
+                .collect_vec())
+            .collect_vec())
+        .collect_vec()
+}
+
+/// collapses the diffs into a single number for each time step
+fn collapse_diffs(stft: &STFTNorms) -> Vec<Sample> {
+    stft
+        .iter()
+        .map(|fft_norm| fft_norm
+            .iter()
+            .map(|channel| channel.iter().sum::<Sample>())
+            .sum::<Sample>())
+        .collect_vec()
 }
 
 // effects: increases prominence of higher frequencies
@@ -102,38 +222,8 @@ fn apply_gamma_log(stft: &STFTNorms, gamma: Sample) -> STFTNorms {
         .collect_vec()
 }
 
-/// this is mostly equivalent to finding the derivative of the stft
-/// derivative = rate of gain of energy over time
-/// onsets have higher derivative values
-fn find_diffs(stft: &STFTNorms) -> STFTDiffs {
-    fn delta(curr: &FFTNorms, next: &FFTNorms) -> FFTDiff {
-        // each norm contains a vector of channel norms
-        // we will compare channel to channel, norm to norm
-        let mut total_diff = 0.0;
-        for (curr_channel, next_channel) in curr.iter().zip_eq(next.iter()) {
-            for (curr_norm, next_norm) in curr_channel.iter().zip_eq(next_channel.iter()) {
-                // ignore a negative derivative
-                total_diff += Sample::max(next_norm - curr_norm, 0.0);
-            }
-        }
-
-        total_diff
-    }
-
-    let mut diffs = stft
-        .iter()
-        .tuple_windows()
-        .map(|(curr, next)| delta(curr, next))
-        .collect_vec();
-
-    // the first differential is 0 since there is no preceding sample
-    diffs.insert(0, 0.0);
-
-    diffs
-}
-
 // reduce the scale so diffs are at most 1.0
-fn normalize_diffs(diffs: &STFTDiffs) -> STFTDiffs {
+fn normalize_diffs(diffs: &[Sample]) -> Vec<Sample> {
     let max = diffs
         .iter()
         .reduce(|a, b| if a > b { a } else { b })
@@ -147,32 +237,45 @@ fn normalize_diffs(diffs: &STFTDiffs) -> STFTDiffs {
         .collect_vec()
 }
 
+/// equivalent to np.mod(`stft` + 0.5, 1) - 0.5
+fn principal_argument(stft: &STFTNorms) -> STFTNorms {
+    stft.iter()
+        .map(|fft_norm| fft_norm
+            .iter()
+            .map(|channel| channel
+                .iter()
+                .map(|&sample| Sample::rem_euclid(sample + 0.5, 1.0) - 0.5)
+                .collect_vec())
+            .collect_vec())
+        .collect_vec()
+}
+
 #[allow(clippy::cast_precision_loss)]
-fn find_local_avgs(diffs: &STFTDiffs, window_size: usize) -> STFTDiffs {
+fn find_local_avgs(samples: &[Sample], window_size: usize) -> Vec<Sample> {
     let mut local_diffs = Vec::new();
 
     // sliding window terms
     let mut r = 0;
     let mut window_sum: Sample = 0.0;
 
-    for l in 0..diffs.len() {
-        while r < diffs.len() && r - l < window_size {
-            window_sum += diffs[r];
+    for l in 0..samples.len() {
+        while r < samples.len() && r - l < window_size {
+            window_sum += samples[r];
             r += 1;
         }
         assert!(r - l > 0, "r - l should never <= 0");
 
-        local_diffs.push(window_sum / (r - l) as Sample);
-        window_sum -= diffs[l];
+        local_diffs.push(window_sum / window_size as Sample);
+        window_sum -= samples[l];
     }
 
-    assert!(local_diffs.len() == diffs.len(), "local_diffs.len() should == diffs.len()");
+    assert!(local_diffs.len() == samples.len(), "local_diffs.len() should == diffs.len()");
 
     local_diffs
 }
 
 #[allow(unused)]
-fn dump_diffs(diffs: &STFTDiffs, output: &str) -> Result<()> {
+fn dump_diffs(diffs: &[Sample], output: &str) -> Result<()> {
     let mut wtr = csv::Writer::from_path(output)?;
     wtr.write_record(["diff", "magnitude"])?;
     for (i, diff) in diffs.iter().enumerate() {
@@ -188,9 +291,22 @@ mod tests {
     use std::path;
 
     #[test]
+    fn test_diffs() {
+        let clip = AudioClip::new(&path::Path::new("test_audio_clips/comboTones.mp3")).unwrap();
+
+        let window_size = 2048;
+        let hop_size = window_size / 4;
+        let stft = clip.stft(window_size, hop_size, hanning_window);
+        let norms = get_norms(&stft);
+        let diffs = find_diffs(&norms);
+
+        assert!(diffs.iter().all(|diff| diff.len() == diffs[0].len()));
+    }
+
+    #[test]
     fn test_onsets() {
         let clip = AudioClip::new(&path::Path::new("test_audio_clips/comboTones.mp3")).unwrap();
-        let onsets = clip.detect_onsets();
+        let onsets = clip.detect_onsets_spectrum();
         let onset_count = onsets
             .iter()
             .count();
@@ -200,9 +316,10 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_split_by_onsets() {
         let clip = AudioClip::new(&path::Path::new("test_audio_clips/comboTones.mp3")).unwrap();
-        let onsets = clip.detect_onsets();
+        let onsets = clip.detect_onsets_phase();
         let clips = clip.split_by_onsets();
         let total_num_samples: usize = clips
             .iter()
@@ -210,7 +327,7 @@ mod tests {
             .sum();
 
         // should be 1 more than the number of onsets because clips are split by onsets
-        assert_eq!(clips.len() - 1, onsets.len());
-        assert_eq!(total_num_samples, clip.num_samples);
+        assert_eq!(clips.len() - 1, onsets.len(), "number of clips should be 1 less than number of onsets");
+        assert_eq!(total_num_samples, clip.num_samples, "total number of samples should equal original number of samples");
     }
 }
